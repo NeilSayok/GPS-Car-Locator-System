10c8621a839ad5d25c8be0d31276b70635c6308e
Sayok Majumder <sayokdeymajumder1998@gmail.com>
2019-05-01 12:53:47 +0530

    Update send_otp.php

--- web files/send_otp.php
+++ web files/send_otp.php
@@ -1,147 +1,133 @@
 <?php
-$servername = "<hostname>";
-$user = "<user name>";
-$password = "<password>";
-$database = "<database name>";
-$conn = mysqli_connect($servername,$user,$password,$database);
- 
- // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}else{
-     $variable = array();
-    $variable['email'] = $_POST['email'];
-    $variable['name'] = "";
-    $variable['otp'] = "";
-    $variable['reg_id'] = "";
 
-    if (isEmailPresent($variable['email'])){
-        $variable['name'] = getName($variable['email']);
-        $variable['otp'] = getOTP($variable['email']);
-        $variable['reg_id'] = getVehId($variable['email']);
-    }
-    else{
-        $variable['name'] = getName($variable['email']);
-        $variable['reg_id'] = getVehId($variable['email']);
-        $variable['otp'] = mt_rand(100000,999999);
-        while (isOTPPresent($variable['otp'])){
-            $variable['otp'] = mt_rand(100000,999999);
-        }
-        
-        $email = $variable['email'] ;
-        $name =  $variable['name'];
-        $otp = $variable['otp'];
-        $vehid = $variable['reg_id'];
+require_once 'connection.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-        $query =  "INSERT INTO OTP (email, name, otp, regid) VALUES ('$email' , '$name' , '$otp' , '$vehid')";
-        $res = mysqli_query($conn,$query);
-         
-        
-     
+require $_SERVER['DOCUMENT_ROOT'] . '/mail/Exception.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/mail/PHPMailer.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/mail/SMTP.php';
+
+$variable = array();
+$variable['email'] = $_POST['email'];
+$variable['name'] = "";
+$variable['otp'] = "";
+$variable['reg_id'] = "";
+
+if (isEmailPresent($variable['email'])) {
+    $variable['name'] = getName($variable['email']);
+    $variable['otp'] = getOTP($variable['email']);
+    $variable['reg_id'] = getVehId($variable['email']);
+} else {
+    $variable['name'] = getName($variable['email']);
+    $variable['reg_id'] = getVehId($variable['email']);
+    $variable['otp'] = mt_rand(100000, 999999);
+    while (isOTPPresent($variable['otp'])) {
+        $variable['otp'] = mt_rand(100000, 999999);
     }
 
-    $subject = "User Verification";
-    $from = "Someemail@someserver.domain";
-    $headers = "MIME-Version: 1.0" . "\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\n"; 
-    $template = file_get_contents("email.html");
-    
-    $template = str_replace('##% name %##',$variable['name'],$template);
-    $template = str_replace('##% otp %##',$variable['otp'],$template);
-    $template = str_replace('##% email %##',$variable['email'],$template);
-    $template = str_replace('##% regid %##',$variable['reg_id'],$template);
-   
-    
-    if(mail($variable['email'],$subject,$template,$headers))
-        echo "Mail_Sent"; 
-    else
-        echo "Mail_not_sent";
-    
+    $email = $variable['email'] ;
+    $name =  $variable['name'];
+    $otp = $variable['otp'];
+    $vehid = $variable['reg_id'];
+
+    $query =  "INSERT INTO OTP (email, name, otp, regid) VALUES ('$email' , '$name' , '$otp' , '$vehid')";
+    $res = mysqli_query($conn, $query);
 }
-   
+
+$template = file_get_contents("email.html");
+
+$template = str_replace('##% name %##', $variable['name'], $template);
+$template = str_replace('##% otp %##', $variable['otp'], $template);
+$template = str_replace('##% email %##', $variable['email'], $template);
+$template = str_replace('##% regid %##', $variable['reg_id'], $template);
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = 0; // 0 = off (for production use) - 1 = client messages - 2 = client and server messages
+$mail->Host = "smtp.gmail.com"; // use $mail->Host = gethostbyname('smtp.gmail.com'); // if your network does not support SMTP over IPv6
+$mail->Port = 587; // TLS only
+$mail->SMTPSecure = 'tls'; // ssl is deprecated
+$mail->SMTPAuth = true;
+$mail->Username = 'projecttravel1995@gmail.com'; // email
+$mail->Password = 'projectSL123'; // password
+$mail->setFrom('otp@projecttraveluniversity.000webhostapp.com', 'Project Travel System'); // From email and name
+$mail->addAddress($variable['email'], $variable['name']); // to email and name
+$mail->Subject = 'Registration OTP (Do not share!)';
+$mail->msgHTML($template); //$mail->msgHTML(file_get_contents('contents.html'), __DIR__); //Read an HTML message body from an external file, convert referenced images to embedded,
+$mail->AltBody = 'Your OTP is '.$variable['otp']; // If html emails is not supported by the receiver, show this body
+// $mail->addAttachment('images/phpmailer_mini.png'); //Attach an image file
+
+if (!$mail->send()) {
+    //echo "Mailer Error: " . $mail->ErrorInfo;
+    echo "Mail_not_sent";
+} else {
+    echo "Mail_Sent";
+}
 
 
 
-
-
-function getOTP($email){
+function getOTP($email)
+{
     $query = "SELECT otp from OTP where email = '".$email."'";
-    if ($sql = mysqli_query($GLOBALS['conn'],$query)){
+    if ($sql = mysqli_query($GLOBALS['conn'], $query)) {
         $row = mysqli_fetch_array($sql);
-        
+
         return $row['otp'];
     }
-    
+
     return "<br>";
 }
 
 
-function getName($email){
+function getName($email)
+{
     $query = "SELECT name from car_location where email = '".$email."'";
-    if ($sql = mysqli_query($GLOBALS['conn'],$query)){
+    if ($sql = mysqli_query($GLOBALS['conn'], $query)) {
         $row = mysqli_fetch_array($sql);
-        
+
         return $row['name'];
     }
-    
+
     return "";
 }
 
-function getVehId($email){
+function getVehId($email)
+{
     $query = "SELECT reg_id from car_location where email = '".$email."'";
-    if ($sql = mysqli_query($GLOBALS['conn'],$query)){
+    if ($sql = mysqli_query($GLOBALS['conn'], $query)) {
         $row = mysqli_fetch_array($sql);
-        
+
         return $row['reg_id'];
     }
-    
+
     return "";
 }
 
-function isEmailPresent($email){
+function isEmailPresent($email)
+{
     $query = "SELECT email FROM OTP where email = '".$email."'";
-    if ($sql =  mysqli_query($GLOBALS['conn'], $query))
-    {
-        if(mysqli_num_rows($sql) >= 1){
-            
+    if ($sql =  mysqli_query($GLOBALS['conn'], $query)) {
+        if (mysqli_num_rows($sql) >= 1) {
             return true;
-        }else{
-           
+        } else {
             return false;
         }
     }
-   
+
     return false;
 }
 
-function isOTPPresent($otp){
+function isOTPPresent($otp)
+{
     $query = "SELECT otp FROM OTP where otp = '".$otp."'";
-    if ($sql =  mysqli_query($GLOBALS['conn'], $query))
-    {
-        if(mysqli_num_rows($sql) >= 1){
-           
+    if ($sql =  mysqli_query($GLOBALS['conn'], $query)) {
+        if (mysqli_num_rows($sql) >= 1) {
             return true;
-        }else{
-           
+        } else {
             return false;
         }
     }
-    
+
     return false;
 }
-
-function test($variable){
-    echo "isEmailPresent()___________________________<br>";
-    isEmailPresent($variable['email'] );
-    echo "<br>isOTPPresent()_________________________<br>";
-    isOTPPresent($variable['otp']);
-    echo "<br>getOTP()_______________________________<br>";
-    getOTP($variable['email'] );
-    echo "<br>getName()______________________________<br>";
-    getName($variable['email'] );
-    echo "_______________________________________<br>";
-
-}
-
-mysqli_close($conn);
-?>
