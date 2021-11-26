bc3640447bd282ee7902a673fdadd1fecee5054c
NeilMajumder <sayokdeymajumder1998@gmail.com>
2021-11-26 23:02:13 +0530

    Removed Files

--- web files/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('servername', 'localhost');
-define('user', '<database user name>');
-define('password', '<database password>');
-define('database', '<database name>');
-
-$conn = mysqli_connect(servername, user, password, database) or die("Connection failed: " . $conn->connect_error);
